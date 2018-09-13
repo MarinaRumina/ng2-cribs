9e61febd3f965acf545e35bac216bac7de0e0028
Marina Rumyansev <aniramrum@gmail.com>
2018-09-13 17:38:26 +0300

    adding add listing form functionality

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -23,6 +23,11 @@ export class CribListingComponent implements OnInit {
         data => this.cribs = data,
         error => this.error = error.statusText
       );
+
+      this.cribsService.newCribSubject.subscribe(
+        data => this.cribs = [data, ...this.cribs] // will add new listing to the beginning of the array and will be shown as first element
+        // data => this.cribs.push(data) // adds new listing to the end of the data array, will be shown at the end
+      );
   }
 
 }
